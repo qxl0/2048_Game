354813da7ea251e32032e2ccc99be27f27b66c6b
Qiang <qxl0@yahoo.com>
2022-03-11 08:55:36 -0500

    Create cells private variable for Grid class

--- Grid.js
+++ Grid.js
@@ -3,14 +3,29 @@ const CELL_SIZE = 20;
 const CELL_GAP = 2;
 
 export default class Grid {
+  #cells;
   constructor(gridElement) {
     gridElement.style.setProperty("--grid-size", GRID_SIZE);
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
     gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
-    createCellElements(gridElement);
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
+      return new Cell(
+        cellElement,
+        index % GRID_SIZE,
+        Math.floor(index / GRID_SIZE)
+      );
+    });
+    console.log(this.cells);
   }
 }
 
+class Cell {
+  constructor(cellElement, x, y) {
+    this.cellElement = cellElement;
+    this.x = x;
+    this.y = y;
+  }
+}
 function createCellElements(gridElement) {
   const cells = [];
   for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
