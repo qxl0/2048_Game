98398bcbabbefc843c1eaa85cf45c8c881789bb9
Qiang <qxl0@yahoo.com>
2022-03-11 16:21:39 -0500

    can set value of tile

--- Tile.js
+++ Tile.js
@@ -1,12 +1,18 @@
 export default class Tile {
   #x;
   #y;
+  #value;
   #tileElement;
   constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
     this.#tileElement = document.createElement("div");
     this.#tileElement.classList.add("tile");
     tileContainer.append(this.#tileElement);
     this.value = value;
+  }
+
+  set value(v) {
+    this.#value = v;
+    this.#tileElement.textContent = v;
   }
 
   set x(value) {
