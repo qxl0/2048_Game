96c5dc99e36c17720dd20d4e38f867f8e82ee372
Qiang <qxl0@yahoo.com>
2022-03-11 08:36:19 -0500

    Add createCellElements()

--- Grid.js
+++ Grid.js
@@ -11,4 +11,13 @@ export default class Grid {
   }
 }
 
-function createCellElements(gridElement) {}
+function createCellElements(gridElement) {
+  const cells = [];
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cells.push(cell);
+    gridElement.append(cell);
+  }
+  return cells;
+}
