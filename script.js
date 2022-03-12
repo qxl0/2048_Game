e1a4d6d4229ce9d0c0d43b5e2ea916ef0956adcd
Qiang <qxl0@yahoo.com>
2022-03-11 21:05:43 -0500

    Add condition to game over

--- script.js
+++ script.js
@@ -15,15 +15,31 @@ function setupInput() {
 async function handleInput(e) {
   switch (e.key) {
     case "ArrowLeft":
+      if (!canMoveLeft()) {
+        setupInput();
+        return;
+      }
       await moveLeft();
       break;
     case "ArrowRight":
+      if (!canMoveRight()) {
+        setupInput();
+        return;
+      }
       await moveRight();
       break;
     case "ArrowUp":
+      if (!canMoveUp()) {
+        setupInput();
+        return;
+      }
       await moveUp();
       break;
     case "ArrowDown":
+      if (!canMoveDown()) {
+        setupInput();
+        return;
+      }
       await moveDown();
       break;
     default:
@@ -35,7 +51,14 @@ async function handleInput(e) {
 
   const newTile = new Tile(gameBoard);
   grid.randomEmptyCell().tile = newTile;
-  setupInput();
+
+  if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+    newTile.waitForTransition(true).then(() => {
+      alert("Game Over");
+    });
+  } else {
+    setupInput();
+  }
 }
 
 function moveUp() {
@@ -81,4 +104,30 @@ function slideTiles(cells) {
       return promises;
     })
   );
+}
+
+function canMoveUp() {
+  return canMove(grid.cellsByColumn);
+}
+
+function canMoveDown() {
+  return canMove(grid.cellsByColumn.map((column) => [...column].reverse()));
+}
+
+function canMoveLeft() {
+  return canMove(grid.cellsByRow);
+}
+
+function canMoveRight() {
+  return canMove(grid.cellsByRow.map((row) => [...row].reverse()));
+}
+function canMove(cells) {
+  return cells.some((group) => {
+    return group.some((cell, index) => {
+      if (index == 0) return false;
+      if (cell.tile == null) return false;
+      const moveToCell = group[index - 1];
+      return moveToCell.canAccept(cell.tile);
+    });
+  });
 }
